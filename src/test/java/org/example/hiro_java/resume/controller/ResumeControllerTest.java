e616084fce2b20c8f7059ccea4ddc6e2fb1356d5
Youngwon <yw7148@naver.com>
2024-11-16 05:13:17 +0900

    feat: 이력서 기능 개발중

--- src/test/java/org/example/hiro_java/resume/controller/ResumeControllerTest.java
+++ src/test/java/org/example/hiro_java/resume/controller/ResumeControllerTest.java
@@ -2,6 +2,7 @@ package org.example.hiro_java.resume.controller;
 
 import org.example.hiro_java.resume.service.ResumeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,11 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
 class ResumeControllerTest {
-    final ResumeService resumeService;
-
-    public ResumeControllerTest(ResumeService resumeService) {
-        this.resumeService = resumeService;
-    }
+    @Autowired
+    ResumeService resumeService;
 
     @Test
     void integTest() {
