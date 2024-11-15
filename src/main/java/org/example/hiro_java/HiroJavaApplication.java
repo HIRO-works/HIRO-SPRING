aab61505a0caaade2bcec737c4f24865564bdcbb
HSH <han41562@gmail.com>
2024-11-15 21:34:50 +0900

    first commit

---
+++ src/main/java/org/example/hiro_java/HiroJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.hiro_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiroJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiroJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hiro_java/HiroJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hiro_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiroJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
