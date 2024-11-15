89b9e030c979dbab84bcf74a4b09cd981263a2f2
HSH <han41562@gmail.com>
2024-11-16 01:11:31 +0900

    feat: profile 별 환경변수 분리

--- src/test/java/org/example/hiro_java/HiroJavaApplicationTests.java
+++ src/test/java/org/example/hiro_java/HiroJavaApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.hiro_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HiroJavaApplicationTests {
 
     @Test
