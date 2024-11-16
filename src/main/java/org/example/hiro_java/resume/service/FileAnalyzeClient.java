d7525cb91c570811bd694b39dfb294adf91f4810
Youngwon <yw7148@naver.com>
2024-11-16 11:30:14 +0900

    feat: 분석 실패시 이력서 삭제하는 로직 추가

--- src/main/java/org/example/hiro_java/resume/service/FileAnalyzeClient.java
+++ src/main/java/org/example/hiro_java/resume/service/FileAnalyzeClient.java
@@ -1,6 +1,7 @@
 package org.example.hiro_java.resume.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.hiro_java.global.exceptions.CustomException;
 import org.example.hiro_java.resume.entity.Resume;
 import org.example.hiro_java.resume.repository.ResumeJpaRepository;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FileAnalyzeClient {
     final static String analyzeUrl = "http://144.24.81.14/api/ai/analyze";
 
@@ -23,25 +25,32 @@ public class FileAnalyzeClient {
     @Transactional
     public void analyzeFile(String resumeId, String userId, String fileKey) {
 
-        Map<String, Object> analyzeReq = new HashMap<>();
-        analyzeReq.put("resume_id", resumeId);
-        analyzeReq.put("user_id", userId);
-        analyzeReq.put("file_path", fileKey);
-        var analyzeRes = restTemplate.postForObject(analyzeUrl, analyzeReq, Map.class);
-        if(analyzeRes == null) throw new CustomException(500, "Failed to analyze");
+        try {
+            Map<String, Object> analyzeReq = new HashMap<>();
+            analyzeReq.put("resume_id", resumeId);
+            analyzeReq.put("user_id", userId);
+            analyzeReq.put("file_path", fileKey);
+            var analyzeRes = restTemplate.postForObject(analyzeUrl, analyzeReq, Map.class);
+            if(analyzeRes == null) throw new CustomException(500, "Failed to analyze");
 
-        var optionalResume = resumeJpaRepository.findById(resumeId);
-        if(optionalResume.isEmpty()) throw new CustomException(500, "Failed to find resume");
+            var optionalResume = resumeJpaRepository.findById(resumeId);
+            if(optionalResume.isEmpty()) throw new CustomException(500, "Failed to find resume");
 
-        var entity = optionalResume.get();
-        String jobCategory = (String) analyzeRes.get("job_category");
-        entity.addJobCategory(jobCategory);
+            var entity = optionalResume.get();
+            String jobCategory = (String) analyzeRes.get("job_category");
+            entity.addJobCategory(jobCategory);
 
-        String language = (String)analyzeRes.get("language");
-        entity.addLanguage(language);
+            String language = (String)analyzeRes.get("language");
+            entity.addLanguage(language);
 
-        entity.analyzeCompleted((String) analyzeRes.get("years"), (String) analyzeRes.get("applicant_name"));
+            entity.analyzeCompleted((String) analyzeRes.get("years"), (String) analyzeRes.get("applicant_name"));
 
-        resumeJpaRepository.save(entity);
+            resumeJpaRepository.save(entity);
+        } catch(Exception e) {
+            log.error(e.getMessage());
+            resumeJpaRepository.deleteById(resumeId);
+
+            throw new CustomException(500, e.getMessage());
+        }
     }
 }
